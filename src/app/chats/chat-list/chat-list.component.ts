62827d47cb58b1bcec75461442494beff9c034d7
songzeng <songzeng62@gmail>
2019-06-27 18:20:30 +0800

    update

--- src/app/chats/chat-list/chat-list.component.ts
+++ src/app/chats/chat-list/chat-list.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ApiService } from '../../services/api.service';
+
 @Component({
   selector: 'app-chat-list',
   templateUrl: './chat-list.component.html',
   styleUrls: ['./chat-list.component.scss']
 })
 export class ChatListComponent implements OnInit {
+  chatList = [];
 
-  constructor() { }
+  constructor(private api: ApiService) {
+  }
 
   ngOnInit() {
+    this.api.getChatList()
+      .subscribe((res: any) => {
+        this.chatList = res.data;
+      });
   }
 
 }
